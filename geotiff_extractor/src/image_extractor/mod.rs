40cd6f75996d82d3903d2ce2a07197a15f59b6a5
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2024-03-07 07:55:58 +0100

    An idea for opening datasets

--- geotiff_extractor/src/image_extractor/mod.rs
+++ geotiff_extractor/src/image_extractor/mod.rs
@@ -1,7 +1,7 @@
 use gdal::errors;
 use gdal::Dataset;
-fn import_datasets(path: &[&str]) -> Result<Vec<Dataset>, errors::GdalError> {
-    Ok(path.into_iter().map(|p| Dataset::open(p)?).collect())
+fn import_datasets(paths: &[&str]) -> Result<Vec<Dataset>, errors::GdalError> {
+    paths.into_iter().map(|p| Dataset::open(p)).collect()
 }
 
 fn mosaic_datasets(_datasets: &[Dataset]) {
