6faea3afd226f76c07ab5feb38ef381d19a6d83e
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2024-03-07 10:06:35 +0100

    Added unit tests

--- geotiff_extractor/src/image_extractor/mod.rs
+++ geotiff_extractor/src/image_extractor/mod.rs
@@ -8,3 +8,30 @@ fn mosaic_datasets(_datasets: &[Dataset]) {
     todo!()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env;
+
+    #[test]
+    fn import_dataset_missing() {
+        let wrong_paths = vec!["/somewhere/where/nothing/exists"];
+
+        let result = import_datasets(&wrong_paths);
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn import_dataset_exists() {
+        let manifest = env::var("CARGO_MANIFEST_DIR").expect("Expected CARGO_MANIFEST_DIR to be set");
+
+        let path = format!("{}/../resources/test/Geotiff/MOSAIC-0000018944-0000037888.tif", manifest);
+
+        let path_vec = vec![path.as_str()];
+
+        let result = import_datasets(&path_vec);
+
+        assert!(result.is_ok_and(|d| d.capacity() == 1))
+    }
+}
