6b97f754e600b645e3522bc50f025e61208d77ed
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2024-03-14 16:22:15 +0100

    Binary for testing

--- geotiff_extractor/src/bin.rs
+++ geotiff_extractor/src/bin.rs
@@ -1,25 +1,27 @@
 use crate::image_extractor::{Datasets, RawDataset};
 use std::env;
 use std::fs;
+use std::path::Path;
+use std::path::PathBuf;
 
 pub mod image_extractor;
 
 fn main() {
-    // let mut current_dir = env::current_dir().expect("Current directory not set.");
+    let mut current_dir = env::current_dir().expect("Current directory not set.");
 
-    // current_dir.push("ressources/test/Geotiff");
+    current_dir.push("resources/test/Geotiff");
 
-    // let paths = fs::read_dir(current_dir).unwrap();
+    let paths = fs::read_dir(&current_dir).unwrap();
 
-    // let paths_vec: Vec<String> = paths
-    //     .into_iter()
-    //     .map(|p| p.unwrap().path().display().to_string())
-    //     .collect();
+    let paths_vec: Vec<String> = paths
+        .into_iter()
+        .map(|p| p.unwrap().path().to_string_lossy().into())
+        .collect();
 
-    // let paths: Vec<&str> = paths_vec.iter().map(AsRef::as_ref).collect();
+    let raw_dataset = RawDataset::import_datasets(&paths_vec).unwrap();
 
-    // let raw_dataset = RawDataset::import_datasets(&paths).unwrap();
+    let output_path = format!("{}/{}", current_dir.to_string_lossy(), "resources/dataset");
 
-    // let mosaic = raw_dataset.mosaic_datasets();
+    let mosaic = raw_dataset.mosaic_datasets(&Path::new(output_path.as_str()));
 }
 
