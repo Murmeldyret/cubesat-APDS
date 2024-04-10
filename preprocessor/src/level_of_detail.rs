0ccb57555336741ae623b857e5c95cb862d60c81
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2024-04-10 13:54:44 +0200

    Work so far

--- preprocessor/src/level_of_detail.rs
+++ preprocessor/src/level_of_detail.rs
@@ -1,9 +1,19 @@
-pub fn calculate_amount_of_levels(resolution: u64, tile_size: u64) -> u64 {
-    walk_lod(tile_size, resolution) + 1
+/// Returns how many layers the lod will consist of.
+pub fn calculate_amount_of_levels(reference_image_resolution: u64, tile_resolution: u64) -> u64 {
+    walk_lod(tile_resolution, reference_image_resolution) + 1
 }
 
-pub fn walk_lod(coverage: u64, tile_size: u64) -> u64 {
-    (((tile_size as f64).sqrt()/(coverage as f64).sqrt()).log2()).ceil() as u64
+/// Returns an integer that describe how many levels should be traveled to reach the optimal lod.
+pub fn walk_lod(pixel_coverage: u64, tile_resolution: u64) -> u64 {
+    (((tile_resolution as f64).sqrt() / (pixel_coverage as f64).sqrt()).log2()).ceil() as u64
+}
+
+/// Converts lod image coordinates to reference image coordinates.
+pub fn calc_offset_from_lod(coordinates: (u64, u64), lod: u64) -> (u64, u64) {
+    (
+        coordinates.0 * 2_u64.pow(lod.try_into().expect("Casting Error")),
+        coordinates.1 * 2_u64.pow(lod.try_into().expect("Casting Error")),
+    )
 }
 
 #[cfg(test)]
@@ -12,21 +22,35 @@ mod tests {
 
     #[test]
     fn lod_total_calc() {
-        assert_eq!(calculate_amount_of_levels(1000*1000, 250*250), 3);
+        assert_eq!(calculate_amount_of_levels(1000 * 1000, 250 * 250), 3);
     }
 
     #[test]
     fn walk_layer_to_match_resolution() {
-        let scale_factor = walk_lod(250*250, 2000*2000);
+        let scale_factor = walk_lod(250 * 250, 2000 * 2000);
         assert_eq!(scale_factor, 3);
 
-        let new_scale_factor = walk_lod(((250*2_i32.pow(scale_factor.try_into().unwrap()))*(250*2_i32.pow(scale_factor.try_into().unwrap()))) as u64, 2000*2000);
+        let new_scale_factor = walk_lod(
+            (250 * 2_u64.pow(scale_factor.try_into().unwrap()))
+                * (250 * 2_u64.pow(scale_factor.try_into().unwrap())),
+            2000 * 2000,
+        );
 
         assert_eq!(new_scale_factor, 0);
     }
 
     #[test]
     fn no_negative_walking() {
-        assert_eq!(walk_lod(4000*4000, 1000*1000), 0);
+        assert_eq!(walk_lod(4000 * 4000, 1000 * 1000), 0);
+    }
+
+    #[test]
+    fn offset_calculation_from_lod() {
+        assert_eq!(calc_offset_from_lod((1000, 1000), 2), (4000, 4000));
+    }
+
+    #[test]
+    fn offset_calculation_from_lod_reference() {
+        assert_eq!(calc_offset_from_lod((1000, 1000), 0), (1000, 1000));
     }
 }
--- preprocessor/src/main.rs
+++ preprocessor/src/main.rs
@@ -1,15 +1,44 @@
+use dotenvy::dotenv;
 use feature_database;
 use feature_extraction;
-use geotiff_lib;
-use dotenvy::dotenv;
+use geotiff_lib::image_extractor;
+use geotiff_lib::image_extractor::Datasets;
+
+use std::env;
+use clap::Parser;
 
 pub mod level_of_detail;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The path to the folder where datasets are stored on disk
+    #[arg(short, long)]
+    dataset_path: String,
+
+    /// The path to the folder which shall contain processed files.
+    #[arg(short, long, default_value_t = String::from("processed_files/"))]
+    output_path: String,
+
+    /// The database url to connect to. Can also be provided by setting environment variable: DATABASE_URL
+    #[arg(long)]
+    database_url: Option<String>,
+
+    /// The tile size which the reference image will be split into
+    #[arg(short, long, default_value_t = 1000)]
+    tile_size: u64,
+}
 
 fn main() {
     dotenv().expect("Could not read .env file");
 
+    let args = Args::parse();
+
     let db_connection = &mut feature_database::db_helpers::setup_database();
 
+    let dataset = image_extractor::RawDataset::import_datasets(&args.dataset_path).expect("Could not open datasets");
 
+    let mosaic = dataset.to_mosaic_dataset(args.output_path);
+
+    // let levels_of_detail = level_of_detail::calculate_amount_of_levels(, TILE_RESOLUTION)
 }
-
