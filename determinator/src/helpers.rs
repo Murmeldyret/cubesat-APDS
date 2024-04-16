f69cd0171ee8f1d93c6b62f1365601aec3fbfd87
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2024-04-16 10:49:02 +0200

    formatter er vred

---
+++ determinator/src/helpers.rs
@@ -0,0 +1,40 @@
+use std::{
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
+
+use diesel::PgConnection;
+
+/// where the program should look for reference image(s) and keypoints
+enum ImgKeypointStore {
+    /// Local.0 images and Local.1 keypoints
+    Local((PathBuf, PathBuf)),
+    Pg(Arc<Mutex<PgConnection>>),
+}
+
+impl ImgKeypointStore {
+    // TODO: methods for selecting images and/or keypoints within a bounding box
+}
+
+enum Coordinates3d {
+    Ellipsoidal { lat: f32, lon: f32, height: f32 },
+    Cartesian { x: f32, y: f32, z: f32 },
+}
+
+enum Coordinates2d {
+    Ellipsoidal { lat: f32, lon: f32 },
+    Cartesian { x: f32, y: f32 },
+}
+
+impl From<Coordinates3d> for Coordinates2d {
+    fn from(value: Coordinates3d) -> Self {
+        match value {
+            Coordinates3d::Ellipsoidal {
+                lat,
+                lon,
+                height: _,
+            } => Coordinates2d::Ellipsoidal { lat, lon },
+            Coordinates3d::Cartesian { x, y, z: _ } => Coordinates2d::Cartesian { x, y },
+        }
+    }
+}
--- determinator/src/main.rs
+++ determinator/src/main.rs
@@ -1,6 +1,50 @@
+use clap::Parser;
 use dotenvy::dotenv;
-use homographier::homographier::{Cmat,raster_to_mat};
+
+use feature_database::schema::keypoint::descriptor;
+use feature_extraction::akaze_keypoint_descriptor_extraction_def;
+use homographier::homographier::{raster_to_mat, Cmat};
+
+use diesel::PgConnection;
+use rgb::alt::BGRA;
+use rgb::{alt::BGRA8, RGBA};
+use std::{
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
+
+pub mod helpers;
+
+///TODO:
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the input image
+    ///
+    /// Image should be 8bit RGBA
+    #[arg(short, long)]
+    img_path: PathBuf,
+    /// Iteration count when performing model estimation
+    #[arg(short, long)]
+    pnp_ransac_iter_count: Option<u32>,
+    /// Whether or not to run this program in demo mode
+    #[arg(long, default_value_t = false)]
+    demo: bool,
+}
 
 fn main() {
-    println!("Hello, world!");
+    // dotenv().expect("failed to read environment variables");
+
+    let args = Args::parse();
+    let path = args
+        .img_path
+        .to_str()
+        .expect("failed to read img_path, it is probably not valid unicode");
+
+    let image = Cmat::<BGRA8>::imread_checked(path, -1).expect("Failed to read image ");
+
+    let (keypoints, descriptors) = akaze_keypoint_descriptor_extraction_def(&image.mat)
+        .expect("AKAZE keypoint extraction failed");
+
+    
 }
