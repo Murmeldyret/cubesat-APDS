0810c819ae13b49e22675d11d1b79ee7da9763a8
Uewotm90 <61500734+Uewotm90@users.noreply.github.com>
2024-04-24 14:44:35 +0200

    looks like its working

--- calibrator/src/helpers.rs
+++ calibrator/src/helpers.rs
@@ -1,7 +1,10 @@
 use std::{ffi::OsStr, path::Path};
 
 use homographier::homographier::Cmat;
-use opencv::imgcodecs::IMREAD_GRAYSCALE;
+use opencv::{
+    core::{Point3f, Size2i, Vector},
+    imgcodecs::IMREAD_GRAYSCALE,
+};
 
 /// Reads all valid images found in a provided path
 /// ## Notes
@@ -27,6 +30,18 @@ pub fn read_images(p: &Path) -> Vec<Cmat<u8>> {
     res
 }
 
+/// Determines whether a file extension is valid for opencv to read as image
 fn valid_img_extension(ext: &OsStr) -> bool {
-    matches!(ext.to_str(), Some(val) if ["png", "jpg", "jpeg", "tif", "tiff"].contains(&val))
+    matches!(ext.to_str(), Some(val) if ["png", "jpg", "jpeg", "tif", "tiff"].contains(&val.to_lowercase().as_str()))
+}
+
+pub fn img_points_from_size(size: &Size2i) -> Vector<Point3f> {
+    let mut output: Vec<Point3f> = Vec::with_capacity(size.width as usize * size.height as usize);
+    for i in 1..=size.width {
+        for j in 1..=size.height {
+            output.push(Point3f::new(i as f32, j as f32, 0f32));
+        }
+    }
+    Vector::from_iter(output)
+
 }
--- calibrator/src/main.rs
+++ calibrator/src/main.rs
@@ -1,10 +1,12 @@
-use std::path::PathBuf;
+use std::{iter, path::PathBuf};
 
 use clap::Parser;
 use helpers::*;
+use homographier::homographier::Cmat;
 use opencv::{
-    calib3d::CALIB_CB_ADAPTIVE_THRESH,
-    core::{Point2f, Vector},
+    calib3d::{calibrate_camera, calibrate_camera_def, draw_chessboard_corners, CALIB_CB_ADAPTIVE_THRESH},
+    core::{MatTraitConst, Point2f, Point3f, Size2i, TermCriteria, TermCriteria_COUNT, TermCriteria_MAX_ITER, TermCriteria_Type, Vec3d, Vector, CV_64FC3},
+    highgui::{destroy_all_windows, imshow, wait_key},
 };
 
 pub mod helpers;
@@ -25,16 +27,61 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-    let images = read_images(&args.img_path);
+    let mut images = read_images(&args.img_path);
     assert!(
         images.len() >= 10,
         "At least 10 images are necesarry to perform image calibration, but only {} was found",
         images.len()
     );
+    // dbg!(images.len());
     let size = opencv::core::Size::new(args.corners[0].into(), args.corners[1].into());
+    let obj_points = iter::repeat(img_points_from_size(&size)).take(images.len()).collect::<Vector<Vector<_>>>();
     let mut corners: Vec<Vector<Point2f>> = Vec::with_capacity(images.len());
+    // dbg!(corners);
+    let mut corner_found: Vec<bool> = Vec::with_capacity(images.len());
 
     for (i, elem) in images.iter().enumerate() {
-        opencv::calib3d::find_chessboard_corners(&elem.mat, size, &mut corners[i], CALIB_CB_ADAPTIVE_THRESH).expect("Opencv erorr").then_some(()).expect("could not find chessboard corners in image, perhaps the image is not of a chessboard pattern");
+        let mut corner: Vector<Point2f> = Vector::new();
+        let res = opencv::calib3d::find_chessboard_corners(
+            &elem.mat,
+            size.clone(),
+            &mut corner,
+            CALIB_CB_ADAPTIVE_THRESH,
+        )
+        .expect("Opencv error");
+        corners.push(corner);
+        corner_found.insert(i, res);
     }
+    let img_points: Vector<Vector<opencv::core::Point_<f32>>> = Vector::from_iter(corners);
+
+    let mut cam_mat = Cmat::<f64>::zeros(3, 3).expect("matrix intiliaztion should not fail");
+    let mut dist_coeffs = Vector::<f64>::new();
+    // these parameters does not really matter, but opencv wants them
+    let mut _rvec = Cmat::<Vec3d>::zeros(3, 3).expect("TODO"); 
+    let mut _tvec = Cmat::<Vec3d>::zeros(3, 3).expect("TODO");
+    
+    
+    let res = calibrate_camera_def(
+        &obj_points,
+        &img_points,
+        images[0].mat.size().expect("epic fail"),
+        &mut cam_mat.mat,
+        &mut dist_coeffs,
+        &mut _rvec.mat,
+        &mut _tvec.mat,
+    );
+    dbg!(res.unwrap());
+    // dbg!(cam_mat.at_2d(0, 0));
+    // dbg!(cam_mat.at_2d(1, 1));
+    // dbg!(cam_mat.at_2d(0, 1));
+    // dbg!(cam_mat.at_2d(2, 2));
+    // dbg!(cam_mat.at_2d(0, 2));
+    // dbg!(cam_mat.at_2d(1, 2));
+    dbg!(dist_coeffs.len());
+    // for (i, elem) in corners.iter().enumerate() {
+    //     draw_chessboard_corners(&mut images[i].mat, size, elem, corner_found[i]).expect("msg");
+    //     imshow("pattern", &images[i].mat).expect("epic fail");
+    //     wait_key(5000).expect("msg");
+    // }
+    // destroy_all_windows().expect("msg");
 }
